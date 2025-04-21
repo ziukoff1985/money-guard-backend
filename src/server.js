f1d62e5dc132d84960807c74bb8fdf364b9dbd64
Borys Ziukov <ziukoff1985@gmail.com>
2025-04-21 21:45:28 +0300

    change cors data 2

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ export const setupServer = () => {
       origin: [
         'http://localhost:5173',
         'http://localhost:5174',
-        'https://test-nine-iota-12.vercel.app/',
+        'https://test-nine-iota-12.vercel.app',
       ],
       credentials: true,
       optionsSuccessStatus: 200,
