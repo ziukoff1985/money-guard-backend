fbda1b7bcf8b142bd5274dca1fcf70f412cb7a30
Borys Ziukov <ziukoff1985@gmail.com>
2025-04-25 13:54:36 +0300

    fix transactions summary logic

--- src/services/transactions.js
+++ src/services/transactions.js
@@ -1,6 +1,8 @@
 import createHttpError from 'http-errors';
 import { TransactionsCollection } from '../db/models/transaction.js';
 import { UsersCollection } from '../db/models/user.js';
+import mongoose from 'mongoose';
+import { Category } from '../db/models/category.js';
 
 export const getAllTransactions = async ({ userId }) => {
   const transactions = await TransactionsCollection.find({ userId });
@@ -25,9 +27,19 @@ export const createTransaction = async (userId, payload) => {
     );
   }
 
+  // Перевіряємо, чи існує категорія
+  const categoryExists = await Category.findById(categoryId);
+  if (!categoryExists) {
+    throw createHttpError(400, 'Category does not exist');
+  }
+
+  // Конвертуємо categoryId у ObjectId
+  const validCategoryId = new mongoose.Types.ObjectId(categoryId);
+
   const transaction = await TransactionsCollection.create({
     ...payload,
     userId,
+    categoryId: validCategoryId,
   });
 
   const newBalance = await calculateBalance(userId);
@@ -37,6 +49,19 @@ export const createTransaction = async (userId, payload) => {
 };
 
 export const updateTransaction = async (userId, transactionId, payload) => {
+  const { categoryId } = payload;
+
+  if (categoryId) {
+    // Перевіряємо, чи існує категорія
+    const categoryExists = await Category.findById(categoryId);
+    if (!categoryExists) {
+      throw createHttpError(400, 'Category does not exist');
+    }
+
+    // Конвертуємо categoryId у ObjectId
+    payload.categoryId = new mongoose.Types.ObjectId(categoryId);
+  }
+
   const updatedTransaction = await TransactionsCollection.findOneAndUpdate(
     { _id: transactionId, userId },
     payload,
